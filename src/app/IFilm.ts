17693d6680d6575dd37e3127373b1d1d8821057d
Alexander <astrgan@gmail.com>
2018-03-17 10:40:14 +1000

    1

---
+++ src/app/IFilm.ts
@@ -0,0 +1,8 @@
+interface IFilm {
+  name_film: string;
+  rating:number;
+  description: string;
+  poster: string;
+  year_of_release: number;
+  path: string;
+}
--- src/app/films.service.ts
+++ src/app/films.service.ts
@@ -1,20 +1,28 @@
 import { Injectable } from '@angular/core';
+import * as Rx from 'rxjs/Rx';
 
 @Injectable()
 export class FilmsService {
 
   films =
     [
-      {name: 'WFM 1'},
-      {name: 'WFM 2'},
-      {name: 'WFM 3'},
-      {name: 'WFM 4'},
-      {name: 'WFM 5'},
+      {name_film: 'Film 1'},
+      {name_film: 'Film 2'},
+      {name_film: 'Film 3'},
+      {name_film: 'Film 4'},
+      {name_film: 'Film 5'},
 
     ];
 
   getFilms(){
     return this.films
+  }
+
+  public connect(url): Rx.Subject<MessageEvent>{
+    if(!this.subject){
+      this.subject = this.create(url);
+      console.log("Successfully Connect:" + url);
+    }
   }
   constructor() { }
 
