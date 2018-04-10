484d6fcc262e05b0e1d9312b57d5c7057beefe29
Alexander <astrgan@gmail.com>
2018-04-10 16:55:45 +1000

    add search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,12 @@ import { FrontPageComponent } from './front-page/front-page.component';
 import { PlayerPageComponent } from './player-page/player-page.component';
 import {RouterModule} from "@angular/router";
 import { PaginationComponent } from './pagination/pagination.component';
+import {SearchComponent} from "./search/search.component";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {CodeHighlighterModule, TabViewModule} from "primeng/primeng";
+import {AutoCompleteModule} from 'primeng/autocomplete';
+import {FormsModule} from "@angular/forms";
+import {CommonModule} from "@angular/common";
 
 const routes = [
   {path: '', component: FrontPageComponent},
@@ -20,11 +26,19 @@ const routes = [
     MovieCardComponent,
     FrontPageComponent,
     PlayerPageComponent,
-    PaginationComponent
+    PaginationComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(routes)
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes),
+
+    CommonModule,
+    FormsModule,
+    AutoCompleteModule,
+    TabViewModule,
+    CodeHighlighterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/search/search.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.scss']
+})
+export class SearchComponent implements OnInit {
+
+  brands: string[] = ['Marrowbone','Jumanji: Welcome to the Jungle','Anthropoid','Kill\'em All','Wonder','Jaguar','Devil\'s Gate','Murder on the Orient Express','Passengers','La consolation'];
+  filteredBrands: any[];
+
+  brand: string;
+
+  constructor() { }
+
+  ngOnInit() {
+
+  }
+
+  filterBrands(event) {
+    this.filteredBrands = [];
+    for(let i = 0; i < this.brands.length; i++) {
+      let brand = this.brands[i];
+      if(brand.toLowerCase().indexOf(event.query.toLowerCase()) == 0) {
+        this.filteredBrands.push(brand);
+      }
+    }
+  }
+
+}
