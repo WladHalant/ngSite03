74076557790a485236d7df325af523f6b94f1ff3
Astrgan <astrgan@gmail.com>
2018-05-11 14:51:59 +1000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 ng build --aot --output-hashing=all
-sshpass -p "angel132" scp dist/* root@93.170.123.54:/var/www/html
+sshpass -p "angel007" scp dist/* root@93.170.123.54:/var/www/html
