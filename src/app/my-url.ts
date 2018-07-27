91c093b3771fae68bdd7de423222d51a53cefdc4
astrgan <astrgan@gmail.com>
2018-07-28 00:35:53 +1000

    add

---
+++ src/app/my-url.ts
@@ -0,0 +1,5 @@
+export class MyUrl {
+  //static URL: string = "http://93.170.123.54/MovieServer/rest";
+  //static URL = "http://astrgan.asuscomm.com:8086/MovieServer/rest";
+  static URL = "http://localhost:8080/MovieServer/rest";
+}
