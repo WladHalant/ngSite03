2af6010d215dfecde8a7756449191764b6546c00
wlad.halant@gmail.com <wlad.halant@gmail.com>
2018-09-19 11:05:31 +1000

    design

--- src/app/my-url.ts
+++ src/app/my-url.ts
@@ -2,5 +2,5 @@ export class MyUrl {
   // static URL: string = "http://93.170.123.54/MovieServer/rest";
   // static URL = "http://astrgan.asuscomm.com:8086/MovieServer/rest";
   // static URL = 'http://localhost:8080/MovieServer/rest';
-  static URL: string = "http://172.106.32.52/MovieServer/rest";
+  static URL = "http://185.206.144.61/MovieServer/rest";
 }
